72cf2b44a7dc808a33bbc0d4b2638e05af475305
Grapes42 <f.inggrapes@gmail.com>
2024-05-14 09:35:24 +1200

    updated setup scripts

--- setup_scripts/key_scripts/install_requirements.sh
+++ setup_scripts/key_scripts/install_requirements.sh
@@ -1,3 +1,8 @@
-sudo apt update
-sudo apt install python3 python3-pip
-pip install rsa pysftp
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run this program as root by typing \"sudo ./install_requirements.sh\""
+	exit
+else
+    apt update
+    apt install python3 python3-pip
+    pip install rsa pysftp
+fi
--- setup_scripts/server_scripts/server_setup.sh
+++ setup_scripts/server_scripts/server_setup.sh
@@ -2,37 +2,42 @@
 # Server side Duckey setup script
 #
 
-#
-# Create jailrooted user
-#
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run this program as root by typing \"sudo ./server_setup.sh\""
+	exit
 
-# Run as root
-sudo su
+else
+  #
+  # Create jailrooted user
+  #
 
-# Add user
-groupadd sftponly # add sftp only group
-useradd -g sftponly -s /bin/false -m -d /home/duckey duckey # add new user, and add them to sftponly
-echo -e "quack\nquack" | passwd duckey # set password for new user
+  # Add user
+  groupadd sftponly # add sftp only group
+  useradd -g sftponly -s /bin/false -m -d /home/duckey duckey # add new user, and add them to sftponly
+  echo -e "quack\nquack" | passwd duckey # set password for new user
 
-# Make root own the user's directories
-chown root: /home/duckey 
-chmod 755 /home/duckey
+  # Make root own the user's directories
+  chown root: /home/duckey 
+  chmod 755 /home/duckey
 
-# Give user limited access to it's directories
-mkdir /home/duckey/passwords
-chmod 755 /home/duckey/passwords
-chown duckey:sftponly /home/duckey/passwords
+  # Give user limited access to it's directories
+  mkdir /home/duckey/passwords
+  chmod 755 /home/duckey/passwords
+  chown duckey:sftponly /home/duckey/passwords
 
-#
-# Configuring SSH
-#
+  #
+  # Configuring SSH
+  #
 
-echo "# Added by Duckey setup script
-Subsystem sftp internal-sftp
-Match Group sftponly
-  ChrootDirectory %h
-  ForceCommand internal-sftp
-  AllowTcpForwarding no
-  X11Forwarding no" >> /etc/ssh/sshd_config
+  echo "# Added by Duckey setup script
+  Subsystem sftp internal-sftp
+  Match Group sftponly
+    ChrootDirectory %h
+    ForceCommand internal-sftp
+    AllowTcpForwarding no
+    X11Forwarding no" >> /etc/ssh/sshd_config
 
-systemctl restart ssh
+  systemctl restart ssh
+
+  echo "Server setup complete"
+fi
