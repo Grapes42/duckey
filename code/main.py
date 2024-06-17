93f99b1d9df4c7aff3fdf3d70afbc238ca683034
Grapes42 <f.inggrapes@gmail.com>
2024-06-17 21:09:11 +1200

    converted to having a class per page to make things manageable

---
+++ code/main.py
@@ -0,0 +1,4 @@
+from pages.get_pass import GetPass
+
+get_pass = GetPass(options=["a", "b", "c", "d"])
+get_pass.run()
--- code/pages/get_pass.py
+++ code/pages/get_pass.py
@@ -2,88 +2,89 @@ import tkinter as tk
 from tkinter import ttk
 import pyperclip
 
+class GetPass:
+    def __init__(self, options):
+        #
+        # Root attributes
+        #
 
-def search(field, list):
+        self.root = tk.Tk()
+        self.root.title("Duckey - Get Pass")
 
-    txt = field.get(1.0, "end-1c")
+        root_width = 500
+        root_height = 300
 
-    searched = []
+        # get the screen dimension
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
 
-    for item in list:
-        if txt in item:
-            searched.append(item)
-
-    list_items.set(searched)
-    
-    #print(searched)
-
-    root.update()
-    root.update_idletasks()
-
-def run_search():
-    search(field=search_input, list=options)
-    root.after(100, run_search)
-
-def copy_pass():
-    tuple = listbox.curselection()
-    index = tuple[0]
-    print(searched[index])
-    pyperclip.copy(searched[index])
+        # Find the center point
+        center_x = int(screen_width/2 - root_width / 2)
+        center_y = int(screen_height/2 - root_height / 2)
 
 
-root = tk.Tk()
-#
-# Root attributes
-#
-root.title("Duckey")
+        self.root.geometry(f'{root_width}x{root_height}+{center_x}+{center_y}')
+        self.root.resizable(False, False)
+        self.root.attributes('-topmost', 1)
+        
+        #
+        # Initial options
+        #
+        self.options = options
+        self.searched = options
 
-root_width = 500
-root_height = 300
+        self.list_items = tk.StringVar(value=self.searched)
 
-# get the screen dimension
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+        #
+        # Root content
+        #
 
-# Find the center point
-center_x = int(screen_width/2 - root_width / 2)
-center_y = int(screen_height/2 - root_height / 2)
+        self.header = ttk.Label(self.root, text="Search")
 
+        self.search_input = tk.Text(self.root, height = 1, width = 30)
 
-root.geometry(f'{root_width}x{root_height}+{center_x}+{center_y}')
-root.resizable(False, False)
-root.attributes('-topmost', 1)
+        self.listbox = tk.Listbox(
+            self.root,
+            height = 10,
+            width = 30,
+            listvariable=self.list_items
+        )
 
-#
-# Root content
-#
+        self.run_search() # runs every 1000ms
 
-# initial vars
-options = ["cat", "mouse", "dog", "bird", "car", "rabbit"]
-searched = options
-list_items = tk.StringVar(value=searched)
+        copy_pass = ttk.Button(self.root, text="Copy Password", command=self.copy_pass)
+        copy_user = ttk.Button(self.root, text="Copy Username")
 
+        self.header.grid(row=0, column=0, pady=2, padx=2)
+        self.search_input.grid(row=1, column=0, pady=2, padx=2)
+        self.listbox.grid(row=2, column=0, pady=2, padx=2)
 
-header = ttk.Label(root, text="Search")
+        copy_pass.grid(row=3, column=0, pady=2, padx=2, sticky=tk.W)
+        copy_user.grid(row=3, column=0, pady=2, padx=2, sticky=tk.E)
+            
+    def search(self, field, list):
+        txt = field.get(1.0, "end-1c")
 
-search_input = tk.Text(root, height = 1, width = 30)
+        searched = []
 
-listbox = tk.Listbox(
-        root,
-        height = 10,
-        width = 30,
-        listvariable=list_items
-    )
+        for item in list:
+            if txt in item:
+                searched.append(item)
 
-run_search() # runs every 1000ms
+        self.list_items.set(searched)
 
-copy_pass = ttk.Button(root, text="Copy Password", command=copy_pass)
-copy_user = ttk.Button(root, text="Copy Username")
+        self.root.update()
+        self.root.update_idletasks()
 
-header.grid(row=0, column=0, pady=2, padx=2)
-search_input.grid(row=1, column=0, pady=2, padx=2)
-listbox.grid(row=2, column=0, pady=2, padx=2)
+    def run_search(self):
+        self.search(field=self.search_input, list=self.options)
+        self.root.after(100, self.run_search)
 
-copy_pass.grid(row=3, column=0, pady=2, padx=2, sticky=tk.W)
-copy_user.grid(row=3, column=0, pady=2, padx=2, sticky=tk.E)
+    def copy_pass(self):
+        tuple = self.listbox.curselection()
+        index = tuple[0]
+        print(self.searched[index])
+        pyperclip.copy(self.searched[index])
 
-root.mainloop()
+    def run(self):
+        self.root.mainloop()
